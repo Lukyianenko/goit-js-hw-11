184a4ffd2f4696a6eba125b0f3daa607f833cfea
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-01-22 12:01:12 +0200

    changes 1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
 import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+const buttonMore = document.querySelector('.load-more');
 
 const KEY = '33017005-3089560dbf89e85a2a48421c2';
 const URL = 'https://pixabay.com/api/';
 
 formEl.addEventListener('submit', onSearchImage);
 
-async function onSearchImage(evt) {
+function onSearchImage(evt) {
     evt.preventDefault();
 
-    const imageName = evt.currentTarget.elements.searchQuery.value;
-    try { 
-        const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true`);
-        const cardArr = responses.data.hits;
+    let imageName = '';
+    gallery.innerHTML = '';
 
+    imageName = evt.currentTarget.elements.searchQuery.value;
+    let page = 1;
+
+    loadImages();
+
+    buttonMore.addEventListener('click', onLoadMore);
+
+  function onLoadMore() {
+      page+=1;
+      console.log(page);
+      loadImages();
+    }
+
+  async function loadImages() {
+      try {
+        const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+
+        const cardArr = responses.data.hits;
+        if(page === responses.data.totalHits % 40) {
+          buttonMore.classList.add('is-hidden');
+        }
+
+        if(cardArr.length === 0) {
+          Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        }
+        
         creatMurkup(cardArr);
+        buttonMore.classList.remove('is-hidden'); 
 } catch(err){
     console.log(err);
 }
- 
+    }
 }
 
-  
+
 function creatMurkup(arrey) {
     const cards = arrey.map(({ 
         webformatURL, 
@@ -54,6 +81,6 @@ function creatMurkup(arrey) {
 </div>`
     }).join('');
 
-    gallery.innerHTML = cards;
+    gallery.insertAdjacentHTML("beforeend", cards);
 }
 
