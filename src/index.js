a90c55eb1d54ed17ab0d5901973b9b348e6722b8
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-01-22 14:02:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
 
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
@@ -7,47 +8,58 @@ const buttonMore = document.querySelector('.load-more');
 
 const KEY = '33017005-3089560dbf89e85a2a48421c2';
 const URL = 'https://pixabay.com/api/';
+let imageName = '';
+let page = 1;
 
 formEl.addEventListener('submit', onSearchImage);
 
 function onSearchImage(evt) {
     evt.preventDefault();
 
-    let imageName = '';
+    imageName = '';
     gallery.innerHTML = '';
+    page = 1;
 
     imageName = evt.currentTarget.elements.searchQuery.value;
-    let page = 1;
 
     loadImages();
 
     buttonMore.addEventListener('click', onLoadMore);
 
   function onLoadMore() {
-      page+=1;
       console.log(page);
       loadImages();
     }
 
   async function loadImages() {
       try {
-        const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+        const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=100`);
 
         const cardArr = responses.data.hits;
-        if(page === responses.data.totalHits % 40) {
-          buttonMore.classList.add('is-hidden');
+
+        if(page === 1 && cardArr.length !== 0) {
+          Notify.success(`Hooray! We found ${responses.data.totalHits} images.`);
         }
+        
+        page+=1;
 
         if(cardArr.length === 0) {
           Notify.failure("Sorry, there are no images matching your search query. Please try again.");
         }
         
         creatMurkup(cardArr);
+        
         buttonMore.classList.remove('is-hidden'); 
+        if(page === responses.data.totalHits / 100 + 1) {
+          buttonMore.classList.add('is-hidden');
+          Notify.info("We're sorry, but you've reached the end of search results.");
+        }
 } catch(err){
     console.log(err);
 }
     }
+
+  
 }
 
 
@@ -63,7 +75,7 @@ function creatMurkup(arrey) {
     }) => {
         return murkup = `
         <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
