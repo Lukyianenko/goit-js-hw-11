e671306e28b980ac1faf6c819195675edd323bad
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-01-24 13:48:21 +0200

    fix function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// import loadImages from './js/search-Image'
-import axios from "axios";
+import loadImages from './js/search-Image'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -9,47 +8,42 @@ const gallery = document.querySelector('.gallery');
 const buttonMore = document.querySelector('.load-more');
 const perPages = 40; 
 
-const KEY = '33017005-3089560dbf89e85a2a48421c2';
-const URL = 'https://pixabay.com/api/';
 let imageName = '';
 let page = 1;
 
 formEl.addEventListener('submit', onSearchImage);
-buttonMore.addEventListener('click', loadImages);
+buttonMore.addEventListener('click', onLoadImage);
 
-async function loadImages() {
-  try {
-      const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPages}`);
+function onLoadImage() {
+  loadImages(imageName, page).then(responses => { 
+    const cardArr = responses.data.hits;
+  console.log(cardArr);
 
-      const cardArr = responses.data.hits;
-
-      if(page === 1 && cardArr.length !== 0) {
-        Notify.success(`Hooray! We found ${responses.data.totalHits} images.`);
-      }
-      
-      page+=1;
-
-      if(cardArr.length === 0) {
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-      }
-      
-      creatMurkup(cardArr);
-      
-      var galleryImg = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
-      galleryImg.refresh();
-      
-      buttonMore.classList.remove('is-hidden'); 
-      const pages = ( responses.data.totalHits - responses.data.totalHits % perPages ) / perPages + 2;
-
-      if(page ===  pages) {
-        buttonMore.classList.add('is-hidden');
-        if(cardArr.length !== 0) {
-          Notify.info("We're sorry, but you've reached the end of search results.");
-        }
+    if(page === 1 && cardArr.length !== 0) {
+      Notify.success(`Hooray! We found ${responses.data.totalHits} images.`);
     }
-} catch(err){
-  console.log(err);
-}
+    
+    page+=1;
+
+    if(cardArr.length === 0) {
+      Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+    
+    creatMurkup(cardArr);
+    
+    var galleryImg = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+    galleryImg.refresh();
+    
+    buttonMore.classList.remove('is-hidden'); 
+    const pages = ( responses.data.totalHits - responses.data.totalHits % perPages ) / perPages + 2;
+
+    if(page ===  pages) {
+      buttonMore.classList.add('is-hidden');
+      if(cardArr.length !== 0) {
+        Notify.info("We're sorry, but you've reached the end of search results.");
+      }
+    }
+  });
 }
 
 function onSearchImage(evt) {
@@ -66,45 +60,7 @@ function onSearchImage(evt) {
     }
     evt.currentTarget.elements.searchQuery.value = '';
 
-    loadImages(imageName, page, creatMurkup, buttonMore);
-
-    
-
-//   async function loadImages() {
-//       try {
-//         const responses = await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPages}`);
-
-//         const cardArr = responses.data.hits;
-
-//         if(page === 1 && cardArr.length !== 0) {
-//           Notify.success(`Hooray! We found ${responses.data.totalHits} images.`);
-//         }
-        
-//         page+=1;
-
-//         if(cardArr.length === 0) {
-//           Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-//         }
-        
-//         creatMurkup(cardArr);
-        
-//         var galleryImg = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
-//         galleryImg.refresh();
-        
-//         buttonMore.classList.remove('is-hidden'); 
-//         const pages = ( responses.data.totalHits - responses.data.totalHits % perPages ) / perPages + 2;
-
-//         if(page ===  pages) {
-//           buttonMore.classList.add('is-hidden');
-//           if(cardArr.length !== 0) {
-//             Notify.info("We're sorry, but you've reached the end of search results.");
-//           }
-          
-//         }
-// } catch(err){
-//     console.log(err);
-// }
-//     }
+    onLoadImage();
 
 }
 
